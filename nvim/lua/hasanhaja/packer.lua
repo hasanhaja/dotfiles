b26f723ad5f23d5256ab45f0b8f59577d5c99364
Hasan Ali <hasanhaja@gmail.com>
2025-04-16 13:23:28 +0100

    chore: update Lazy plugins

--- nvim/lua/hasanhaja/packer.lua
+++ nvim/lua/hasanhaja/packer.lua
@@ -22,11 +22,15 @@ return require('packer').startup(function(use)
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
+  -- Don't need this anymore
+  -- use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
+
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
 
+
+  -- Skipped
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  requires = {
